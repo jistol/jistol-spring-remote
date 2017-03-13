7d86945dc8f76a392ca5ab374009c7f2ede5ad1a
kimjh <kimjh@spectra.co.kr>
2017-03-13 16:58:44 +0900

    Add annotaion @EnableRemoteType

---
+++ src/main/java/io/github/jistol/remote/EnableRemoteType.java
@@ -0,0 +1,12 @@
+package io.github.jistol.remote;
+
+import org.springframework.context.annotation.Import;
+
+/**
+ * Created by kimjh on 2017-03-13.
+ */
+@Import(RemoteConfiguration.class)
+public @interface EnableRemoteType
+{
+
+}
