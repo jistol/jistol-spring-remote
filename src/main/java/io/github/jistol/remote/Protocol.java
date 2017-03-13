24bb07047663267f3d071b29be1061267b2ad3b2
kimjh <kimjh@spectra.co.kr>
2017-03-13 16:15:27 +0900

    init custom spring remote

---
+++ src/main/java/io/github/jistol/remote/Protocol.java
@@ -0,0 +1,49 @@
+package io.github.jistol.remote;
+
+import org.springframework.beans.FatalBeanException;
+import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
+import org.springframework.remoting.rmi.RmiServiceExporter;
+
+import java.rmi.RemoteException;
+
+/**
+ * Created by kimjh on 2017-03-07.
+ */
+public enum Protocol
+{
+    HTTP {
+        @Override
+        public Object getServiceExporter(Object bean, String beanName, RemoteType remoteType) {
+            HttpInvokerServiceExporter httpInvokerServiceExporter = new HttpInvokerServiceExporter();
+            httpInvokerServiceExporter.setServiceInterface(remoteType.serviceInterface());
+            httpInvokerServiceExporter.setService(bean);
+            httpInvokerServiceExporter.afterPropertiesSet();
+            return httpInvokerServiceExporter;
+        }
+    },
+
+    RMI {
+        @Override
+        public Object getServiceExporter(Object bean, String beanName, RemoteType remoteType) {
+            RmiServiceExporter rmiServiceExporter = new RmiServiceExporter();
+            rmiServiceExporter.setServiceInterface(remoteType.serviceInterface());
+            rmiServiceExporter.setService(bean);
+            rmiServiceExporter.setServiceName(beanName);
+            if (remoteType.port() != -1)
+            {
+                rmiServiceExporter.setServicePort(remoteType.port());
+            }
+            try
+            {
+                rmiServiceExporter.afterPropertiesSet();
+            }
+            catch (RemoteException e)
+            {
+                throw new FatalBeanException("Exception initializing RmiServiceExporter", e);
+            }
+            return rmiServiceExporter;
+        }
+    };
+
+    abstract public Object getServiceExporter(Object bean, String beanName, RemoteType remoteType);
+}
---
+++ src/main/java/io/github/jistol/remote/RemoteType.java
@@ -0,0 +1,23 @@
+package io.github.jistol.remote;
+
+import org.springframework.beans.factory.annotation.Required;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by kimjh on 2017-03-07.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface RemoteType
+{
+    Protocol protocol() default Protocol.HTTP;
+
+    int port() default -1;
+
+    @Required
+    Class<?> serviceInterface();
+}
